3b7a22168cd0f5009fdcd1bfb29af0ad19e7acc9
Bohdan Stetsiuk <rudebs@outlook.com>
2025-06-25 22:10:44 +0000

    update home and me pages

--- src/pages/Me/index.tsx
+++ src/pages/Me/index.tsx
@@ -2,24 +2,48 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "../../components/Button";
 import { useAuth } from "../../context/AuthContext/useAuth";
 
+import "./index.scss";
+import { LoadingSpinner } from "../../components/LoadingSpinner";
+
 export const Me = () => {
   const navigate = useNavigate();
-  const { logout } = useAuth();
+  const { logout, user, isAuthenticated, isLoginPending, isLoading } =
+    useAuth();
+
+  if (isLoginPending || isLoading) {
+    return (
+      <div className="center-div">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated || !user) {
+    navigate("/", { replace: true });
+
+    return null;
+  }
 
   return (
-    <div className="me-page">
-      <h1>My Profile</h1>
-      <p>This is the user profile page.</p>
+    <div className="me page">
+      <div className="half">
+        <h1>Hi {user.name}!</h1>
 
-      <Button
-        kind="danger"
-        onClick={() => {
-          navigate("/", { replace: true });
-          logout();
-        }}
-      >
-        Log Out
-      </Button>
+        <p>
+          Welcome to your account page. Here you can manage your profile, view
+          your events, and log out.
+        </p>
+
+        <Button
+          kind="danger"
+          onClick={() => {
+            navigate("/", { replace: true });
+            logout();
+          }}
+        >
+          Log Out
+        </Button>
+      </div>
     </div>
   );
 };
